80b504bcf19a71008ca3c9a3fcfe0c83f277d74f
Kyle Gordy <kyle@kylegordydesign.com>
2015-10-06 18:26:20 -0400

    update dev dependencies, add lost grid

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,29 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var mqpacker = require('css-mqpacker');
 var csswring = require('csswring');
+var lost = require('lost');
+
+var paths = {
+  cssSource: 'src/css/',
+  cssDestination: 'dist/css/'
+};
 
 // PostCSS magic
 gulp.task('css', function () {
     var processors = [
+        lost,
         autoprefixer({browsers: ['last 2 version']}),
         mqpacker,
         csswring
     ];
-    return gulp.src('src/css/*.css')
+    return gulp.src(paths.cssSource + '**/*.css')
         .pipe(postcss(processors))
-        .pipe(gulp.dest('dist/css/'));
+        .pipe(gulp.dest(paths.cssDestination));
 });
 
 // Watch files for changes
 gulp.task('watch', function() {
-    gulp.watch('src/css/*.css', ['css']);
+    gulp.watch(paths.cssSource + '**/*.css', ['css']);
 });
 
 // Default task
