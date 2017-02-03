e91b699c214d07358fe7b57bfe89be94113dab39
Kyle Gordy <kyletgordy@gmail.com>
2017-02-02 23:33:43 -0500

    nuke old jekyll install, add temporary splash page

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,13 @@
-// Required plugins
 var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var mqpacker = require('css-mqpacker');
-var csswring = require('csswring');
-var lost = require('lost');
+var sass = require('gulp-sass');
 
-var paths = {
-  cssSource: 'src/css/',
-  cssDestination: 'dist/css/'
-};
-
-// PostCSS magic
-gulp.task('css', function () {
-    var processors = [
-        lost,
-        autoprefixer({browsers: ['last 2 version']}),
-        mqpacker,
-        csswring
-    ];
-    return gulp.src(paths.cssSource + '**/*.css')
-        .pipe(postcss(processors))
-        .pipe(gulp.dest(paths.cssDestination));
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
 });
 
-// Watch files for changes
-gulp.task('watch', function() {
-    gulp.watch(paths.cssSource + '**/*.css', ['css']);
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
 });
-
-// Default task
-gulp.task('default', ['css', 'watch']);
